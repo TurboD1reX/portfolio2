851d4aeadb6a726e33c6f7d5150066bb6724ebfb
Yehor Kryvoi <126288081+TurboD1reX@users.noreply.github.com>
2024-03-20 08:05:28 +0100

    Add files via upload

--- php/contact.php
+++ php/contact.php
@@ -1,37 +1,20 @@
 <?php
-$servename="localhost";
-$username="root";
-$password= "";
-$database_name="portfolio_EK";
+$conn = new mysqli('localhost', 'root', '', 'portfolio_ek');
+if (isset($_POST["first_name"]) && isset($_POST["phone_number"]) && isset($_POST["e-mail"]) && isset($_POST["company_name"]) && isset($_POST["content"])) {
 
-$conn = new mysqli($servename,$username,$password,$database_name); 
-if($conn->connect_error){
-    echo"Can not connect with database :(".$conn->connect_error;
-}    
+    $first_name = $_POST["first_name"];
+    $phone_number = $_POST["phone_number"];
+    $email = $_POST["e-mail"];
+    $company_name = $_POST["company_name"];
+    $content = $_POST["content"];
 
-if(isset($_POST["first_name"])){
-
-$first_name = $_POST["first_name"];
-$last_name = $_POST["last_name"];       
-$phone_number = $_POST["phone_number"];
-$email= $_POST["email"];
-$company_name = $_POST["company_name"];
-$content = $_POST["content"]; 
-
-
-if($first_name != ""|| $last_name !=""|| $phone_number !=""|| $email !=""|| $company_name != ""|| $content != ""){
-    $add ="INSERT INTO main_table(main_id ,first_name,last_name,phone_number,email,company_name,content)
-    VALUES ('NULL','$first_name','$last_name','$phone_number','$email','$company_name','$content');";
-
+    if ($first_name != "" && $phone_number != "" && $email != "" && $company_name != "" && $content != "") {
+        $add = "INSERT INTO main_table (main_id, first_name, phone_number, email, company_name, content) 
+                VALUES (NULL, '$first_name', '$phone_number', '$email', '$company_name', '$content')";
+                
+        if ($conn->query($add) === FALSE) { echo "Error: " . $add . "<br>" . $conn->error; } 
+    }
 }
-$send = mysqli_query($conn, $add);
-if($send){
-    echo "<b>Sucessfully!</b>";
-}else {}
 
-
-
-}
 $conn->close();
-
 ?>
